31290dd82c3dfa38bc92a60e02efacbff4982fd9
Rene Grempel <grempelrene@gmail.com>
2026-01-23 15:17:29 +0300

    Update and rename main/task_1.py to main.py

---
+++ main.py
@@ -0,0 +1,236 @@
+# Task 1
+
+class Student:
+    def __init__(self, name: str, surname: str, gender: str):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+
+class Mentor:
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+    pass
+
+
+# Проверка
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+print(lecturer.courses_attached)     # []
+print(reviewer.courses_attached)     # []
+
+
+# Task 2
+
+class Student:
+    def __init__(self, name: str, surname: str, gender: str):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecture(self, lecturer, course: str, grade: int):
+        if (
+            isinstance(lecturer, Lecturer)
+            and course in self.courses_in_progress
+            and course in lecturer.courses_attached
+            and 1 <= grade <= 10
+        ):
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name: str, surname: str):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course: str, grade: int):
+        if (
+            isinstance(student, Student)
+            and course in self.courses_attached
+            and course in student.courses_in_progress
+            and 1 <= grade <= 10
+        ):
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+# Проверка
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'C++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
+
+
+# Task 3
+
+def average_grade(grades: dict) -> float:
+    all_grades = []
+    for grade_list in grades.values():
+        all_grades.extend(grade_list)
+    return sum(all_grades) / len(all_grades) if all_grades else 0
+
+
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecture(self, lecturer, course, grade):
+        if (
+            isinstance(lecturer, Lecturer)
+            and course in self.courses_in_progress
+            and course in lecturer.courses_attached
+            and 1 <= grade <= 10
+        ):
+            lecturer.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return (
+            f"Имя: {self.name}\n"
+            f"Фамилия: {self.surname}\n"
+            f"Средняя оценка за домашние задания: {average_grade(self.grades):.1f}\n"
+            f"Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n"
+            f"Завершенные курсы: {", ".join(self.finished_courses)}"
+        )
+
+    def __lt__(self, other):
+        return average_grade(self.grades) < average_grade(other.grades)
+
+    def __eq__(self, other):
+        return average_grade(self.grades) == average_grade(other.grades)
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def __str__(self):
+        return (
+            f"Имя: {self.name}\n"
+            f"Фамилия: {self.surname}\n"
+            f"Средняя оценка за лекции: {average_grade(self.grades):.1f}"
+        )
+
+    def __lt__(self, other):
+        return average_grade(self.grades) < average_grade(other.grades)
+
+    def __eq__(self, other):
+        return average_grade(self.grades) == average_grade(other.grades)
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if (
+            isinstance(student, Student)
+            and course in self.courses_attached
+            and course in student.courses_in_progress
+            and 1 <= grade <= 10
+        ):
+            student.grades.setdefault(course, []).append(grade)
+        else:
+            return "Ошибка"
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
+
+#  Testing (Task 4)
+
+student1 = Student("Anna", "Ivanova", "F")
+student2 = Student("Max", "Petrov", "M")
+
+lecturer1 = Lecturer("Ivan", "Ivanov")
+lecturer2 = Lecturer("Sergey", "Smirnov")
+
+reviewer1 = Reviewer("Petr", "Petrov")
+reviewer2 = Reviewer("Olga", "Sidorova")
+
+student1.courses_in_progress += ["Python"]
+student2.courses_in_progress += ["Python"]
+
+lecturer1.courses_attached += ["Python"]
+lecturer2.courses_attached += ["Python"]
+
+reviewer1.courses_attached += ["Python"]
+reviewer2.courses_attached += ["Python"]
+
+reviewer1.rate_hw(student1, "Python", 9)
+reviewer1.rate_hw(student2, "Python", 8)
+reviewer2.rate_hw(student1, "Python", 10)
+reviewer2.rate_hw(student2, "Python", 9)
+
+student1.rate_lecture(lecturer1, "Python", 8)
+student1.rate_lecture(lecturer2, "Python", 9)
+student2.rate_lecture(lecturer1, "Python", 9)
+student2.rate_lecture(lecturer2, "Python", 10)
+
+print(student1)
+print(student2)
+print(lecturer1)
+print(lecturer2)
+print(reviewer1)
+print(reviewer2)
+
+print(average_hw_grade([student1, student2], "Python"))
+print(average_lecture_grade([lecturer1, lecturer2], "Python"))
