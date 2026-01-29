146cb71286d7179782bd8549d4ae59e0ce5c3f6a
Rene Grempel <grempelrene@gmail.com>
2026-01-29 16:16:55 +0300

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -32,3 +32,37 @@ cook_book = read_cook_book("recipes.txt")
 print(cook_book)
 
 
+# Task 2
+
+
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    shop_list = {}
+
+    for dish in dishes:
+        if dish not in cook_book:
+            continue
+
+        for ingredient in cook_book[dish]:
+            name = ingredient["ingredient_name"]
+            measure = ingredient["measure"]
+            quantity = ingredient["quantity"] * person_count
+
+            if name in shop_list:
+                shop_list[name]["quantity"] += quantity
+            else:
+                shop_list[name] = {
+                    "measure": measure,
+                    "quantity": quantity
+                }
+
+    return shop_list
+
+
+# Example
+shop_list = get_shop_list_by_dishes(
+    ["Запеченный картофель", "Омлет"],
+    2,
+    cook_book
+)
+
+print(shop_list)
